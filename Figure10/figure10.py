4dd6ea4ad871578c708b662fddf264942a9dbcc9
ZhanGHanG9991 <zhanghang21@m.fudan.edu.cn>
2021-12-07 01:47:25 +0000

    figure10 ok

---
+++ Figure10/figure10.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append('/home/zh/opt/project/pycharmproject/cd-diagram')
+from main import draw
+
+draw("mini_diff_feature_num.csv", "/home/zh/opt/project/pycharmproject/cd-diagram/Figure10/", "figure10.png")
--- main.py
+++ main.py
@@ -275,7 +275,7 @@ def form_cliques(p_values, nnames):
     return networkx.find_cliques(g)
 
 
-def draw_cd_diagram(df_perf=None, alpha=0.05, title=None, labels=False):
+def draw_cd_diagram(path, name, df_perf=None, alpha=0.05, title=None, labels=False):
     """
     Draws the critical difference diagram given the list of pairwise classifiers that are
     significant or not
@@ -298,7 +298,7 @@ def draw_cd_diagram(df_perf=None, alpha=0.05, title=None, labels=False):
         }
     if title:
         plt.title(title,fontdict=font, y=0.9, x=0.5)
-    plt.savefig('cd-diagram.png',bbox_inches='tight')
+    plt.savefig(path + name,bbox_inches='tight')
 
 def wilcoxon_holm(alpha=0.05, df_perf=None):
     """
@@ -379,5 +379,6 @@ def wilcoxon_holm(alpha=0.05, df_perf=None):
     return p_values, average_ranks, max_nb_datasets
 
 # df_perf = pd.read_csv('DefaultvsTunedvsEnsembleCritDiffAcc.csv',index_col=False)
-df_perf = pd.read_csv('example.csv',index_col=False)
-draw_cd_diagram(df_perf=df_perf, title='Accuracy', labels=True)
+def draw(csv_name, path, name):
+    df_perf = pd.read_csv(csv_name,index_col=False)
+    draw_cd_diagram(path=path, name=name, df_perf=df_perf, title='Accuracy', labels=True)
